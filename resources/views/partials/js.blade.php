a3cedd9cbd398c58edc02bf0a118cf3b1db9eb8d
Evgenii Eliseev <jeka.vinny@gmail.com>
2020-05-16 22:17:19 +0300

    added CSP support, updated jquery and jquery pjax

--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -1,3 +1,3 @@
 @foreach($js as $j)
-<script nonce="{{ csp_nonce() }}" src="{{ admin_asset ("$j") }}"></script>
+<script src="{{ admin_asset ("$j") }}"></script>
 @endforeach
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -17,14 +17,14 @@
 
         @if(config('admin.enable_menu_search'))
         <!-- search form (Optional) -->
-        <form class="sidebar-form" style="overflow: initial;" onsubmit="return false;">
+        <form class="sidebar-form sidebar-form-additions" onsubmit="return false;">
             <div class="input-group">
                 <input type="text" autocomplete="off" class="form-control autocomplete" placeholder="Search...">
               <span class="input-group-btn">
                 <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
                 </button>
               </span>
-                <ul class="dropdown-menu" role="menu" style="min-width: 210px;max-height: 300px;overflow: auto;">
+                <ul class="dropdown-menu dropdown-menu-additions" role="menu">
                     @foreach(Admin::menuLinks() as $link)
                     <li>
                         <a href="{{ admin_url($link['uri']) }}"><i class="fa {{ $link['icon'] }}"></i>{{ admin_trans($link['title']) }}</a>
